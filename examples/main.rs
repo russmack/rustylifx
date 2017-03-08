e5bdf23d6c97915c4b526b285af19c54bdbfcbde
Russell Mackenzie <russmackdev@gmail.com>
2017-03-08 00:01:40 +0000

    Acknowledge responses.

--- examples/main.rs
+++ examples/main.rs
@@ -8,40 +8,41 @@ use std::thread;
 fn main() {
     println!("Started.");
     let resp = rustylifx::request::get_service().unwrap();
-    println!("\nState service: {:?}", resp);
+    println!("\nState service:");
+    display_response(resp);
     println!("==========\n");
-    //display_response(resp);
 
     thread::sleep(Duration::from_millis(1000));
     let resp2 = rustylifx::request::get_device_state().unwrap();
-    println!("\nState: {:?}", resp2);
+    println!("\nState:");
+    display_response(resp2);
     println!("==========");
-    //display_response(resp2);
 
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::RED, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::RED, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::GREEN, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::GREEN, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::BLUE, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::BLUE, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
 
 
-    rustylifx::request::set_device_state(colour::BEIGE, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::BEIGE, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::CHARTREUSE, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::CHARTREUSE, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::CORAL, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::CORAL, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::CORNFLOWER, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::CORNFLOWER, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::CRIMSON, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::CRIMSON, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::DEEP_SKY_BLUE, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::DEEP_SKY_BLUE, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    rustylifx::request::set_device_state(colour::SLATE_GRAY, 1000, 0);
+    let _ = rustylifx::request::set_device_state(colour::SLATE_GRAY, 1000, 0);
     thread::sleep(Duration::from_millis(1000));
-    //rustylifx::request::set_device_state(colour::BEIGE, 65000, 65000, 1000, 0);
+    let resp = rustylifx::request::set_device_state(colour::BEIGE, 1000, 0);
+    display_response(resp.unwrap());
 
     println!("\nFinished.");
 }
@@ -54,15 +55,11 @@ fn display_response(resp: rustylifx::response::Response) {
     println!("Firmware: {:?}", resp.firmware);
     // packed byte
     println!("Sequence num: {:?}", resp.sequence_number);
-
     println!("Reserved_1 (timestamp?): {:?}", resp.reserved_1);
-    
     println!("Message type: {:?}", resp.message_type);
-
     println!("Reserved_2: {:?}", resp.reserved_2);
 
     //println!("Service: {:?}", resp.service);
     //println!("Port: {:?}", resp.port);
     //println!("Unknown: {:?}", resp.unknown);
-    
 }
